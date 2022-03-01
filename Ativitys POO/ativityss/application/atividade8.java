5aa59fbbed4378a427353f8053890b4172697435
Thauan de Oliveira Ramos <96343663+thauanoliveiraa@users.noreply.github.com>
2022-03-01 13:08:26 -0300

    Java

---
+++ Ativitys POO/ativityss/application/atividade8.java
@@ -0,0 +1,15 @@
+package application;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class atividade8 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+         Locale.setDefault(Locale.US);
+
+         /* A dona de um pensionato possui 10 quartos para alugar para estudantes, sendo esses quartos identificados pelos números 0 a 9
+         
+         Fazer um programa que inicie com todos os 10 quartos vazios, e depois leia uma quantidade N representando o número de estudantes que vão alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos N estudantes. Para cada registro de aluguel, informar o nome e email do estudante, bem como qual dos quartos ele escolheu (de 0 a 9). Suponha que seja escolhido um quarto vago. Ao final, seu programa deve imprimir um relatório de todas ocupações do pensionato, por ordem de quarto, conforme exemplo */
+        
+    }
+}
