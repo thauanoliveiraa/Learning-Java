ed1d0090aa052d6898ec37aea7241b62f8224a54
Thauan de Oliveira Ramos <96343663+thauanoliveiraa@users.noreply.github.com>
2022-03-02 14:04:50 -0300

    Java

--- Ativitys POO/ativityss/application/atividade8.java
+++ Ativitys POO/ativityss/application/atividade8.java
@@ -1,6 +1,7 @@
 package application;
 import java.util.Scanner;
 import java.util.Locale;
+import entities.RoomPension;
 
 public class atividade8 {
     public static void main(String[] args) {
@@ -11,5 +12,21 @@ public class atividade8 {
          
          Fazer um programa que inicie com todos os 10 quartos vazios, e depois leia uma quantidade N representando o número de estudantes que vão alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos N estudantes. Para cada registro de aluguel, informar o nome e email do estudante, bem como qual dos quartos ele escolheu (de 0 a 9). Suponha que seja escolhido um quarto vago. Ao final, seu programa deve imprimir um relatório de todas ocupações do pensionato, por ordem de quarto, conforme exemplo */
         
+         System.out.print("How many people will rent rooms ? ");
+         int n = scan.nextInt();
+
+         for(int i = 0; i < n; i++){
+             // OS DADOS DE CADASTRO PRECISAM FICAR AQUI DENTRO
+            System.out.printf("\n\n\nDATA %d REGISTRATION", i);
+            System.out.print("\nWhats the name of people ? ");
+            String name = scan.nextLine();
+            name = scan.nextLine(); 
+            System.out.print("\nWhats the email of people ? ");
+            String email = scan.nextLine();
+            System.out.print("\nWhats the bedroom of people ? ");
+            int bedroom = scan.nextInt();
+            RoomPension roompension = new RoomPension(name, email, bedroom);
+         }
+         
     }
 }
--- Ativitys POO/ativityss/entities/RoomPension.java
+++ Ativitys POO/ativityss/entities/RoomPension.java
@@ -1,5 +1,33 @@
 package entities;
 
 public class RoomPension {
-    
+    private String name;
+    private String email;
+    private int bedroom;
+
+    public RoomPension(String name, String email, int bedroom){
+        this.name = name;
+        this.email = email;
+        this.bedroom = bedroom;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public int getBedroom(){
+        return bedroom;
+    }
 }
