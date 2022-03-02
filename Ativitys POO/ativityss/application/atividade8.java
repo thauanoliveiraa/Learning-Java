569ce9923fa064e5571eae29b98bdf96406f9bcb
Thauan de Oliveira Ramos <96343663+thauanoliveiraa@users.noreply.github.com>
2022-03-02 16:50:06 -0300

    Java

--- Ativitys POO/ativityss/application/atividade8.java
+++ Ativitys POO/ativityss/application/atividade8.java
@@ -7,7 +7,6 @@ public class atividade8 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
          Locale.setDefault(Locale.US);
-         int choice = 0;
 
          /* A dona de um pensionato possui 10 quartos para alugar para estudantes, sendo esses quartos identificados pelos números 0 a 9
          
@@ -18,7 +17,7 @@ public class atividade8 {
 
          RoomPension[] vect = new RoomPension[10];
 
-         for(int i = 0; i < 10; i++){
+         for(int i = 0; i < n; i++){
              // OS DADOS DE CADASTRO PRECISAM FICAR AQUI DENTRO
             System.out.printf("\n\n\nDATA %d REGISTRATION", i+1);
             System.out.print("\nWhats the name of people ? ");
@@ -28,24 +27,15 @@ public class atividade8 {
             String email = scan.nextLine();
             System.out.print("\nWhats the bedroom of people ? ");
             int bedroom = scan.nextInt();
-            vect[i] = new RoomPension(name, email, bedroom);
+            vect[bedroom] = new RoomPension(name, email, bedroom);
          }
 
-        do{
-
-         System.out.println("\n\n\nWhat would you like to do now ?");
-         System.out.print("1 - View all registers\n2 - View all names\n3 - View all emails\n4 - View all badrooms\n5 - Exit\n\nInsert your choice : ");
-         choice = scan.nextInt();
-
-         switch(choice){
-            case 1:
-                if(bedroom[i] > bedroom[i]){
-
-                }
-                System.out.print("Name : ");
-         }
-
-        }while(choice != 6);
+         for(int i = 0; i < 10; i++){
+            if(vect[i] != null){
+                int bedroom;
+                System.out.println("All data : " +vect[bedroom]);
+            }
+        }
 
         scan.close();;
     }
