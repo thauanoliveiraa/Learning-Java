6753728a95499b175ffc13f2d2e75e523e753c68
Thauan de Oliveira Ramos <96343663+thauanoliveiraa@users.noreply.github.com>
2022-03-09 09:50:12 -0300

    Java POO

--- Ativitys POO/ativityss/application/atividade9.java
+++ Ativitys POO/ativityss/application/atividade9.java
@@ -1,5 +1,18 @@
 package application;
+import java.util.Scanner;
+import java.util.Locale;
 
 public class atividade9 {
-    
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        /* Fazer um programa para ler um numero inteiro N e depois os dados (id, nome e salario) de N funcionarios. Nao deve haver repetição de id
+        
+        Em seguida, efetuar o aumento de X% no salario de um determinado funcionario. Para isso, o programa deve ler um id e o valor X. Se o id informado não existir, mostrar uma mensagem e abortar a operação. Ao final, mostrar a listagem atualizada dos funcionarios, conforme os exemplos.
+        
+        Lembre-se de aplicar a técnica de encapsulamento para não permitir que o salário possa ser mudado livremente. Um salario pode aumentado com base em uma operação de aumento por uma porcentagem */
+
+        scan.close();
+    }
 }
---
+++ Ativitys POO/ativityss/entities/EmployeeData.java
@@ -0,0 +1,15 @@
+package entities;
+
+public class EmployeeData {
+    private int employeeID;
+    private String employeeName;
+    private double employeeWage;
+
+    public EmployeeData(int employeeID, String employeeName, double employeeWage){
+        this.employeeID = employeeID;
+        this.employeeName = employeeName;
+        this.employeeWage = employeeWage;
+
+    }
+    
+}
