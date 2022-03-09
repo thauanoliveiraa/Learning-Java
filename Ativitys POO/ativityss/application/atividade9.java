7af4eb5be8797e1b8c4f785547f7bf7a2ad814fa
Thauan de Oliveira Ramos <96343663+thauanoliveiraa@users.noreply.github.com>
2022-03-09 10:09:52 -0300

    Java POO

--- Ativitys POO/ativityss/application/atividade9.java
+++ Ativitys POO/ativityss/application/atividade9.java
@@ -1,6 +1,7 @@
 package application;
 import java.util.Scanner;
 import java.util.Locale;
+import entities.EmployeeData;
 
 public class atividade9 {
     public static void main(String[] args) {
@@ -12,6 +13,9 @@ public class atividade9 {
         Em seguida, efetuar o aumento de X% no salario de um determinado funcionario. Para isso, o programa deve ler um id e o valor X. Se o id informado não existir, mostrar uma mensagem e abortar a operação. Ao final, mostrar a listagem atualizada dos funcionarios, conforme os exemplos.
         
         Lembre-se de aplicar a técnica de encapsulamento para não permitir que o salário possa ser mudado livremente. Um salario pode aumentado com base em uma operação de aumento por uma porcentagem */
+
+        System.out.print("How many employees wil be registered ? ");
+        int employeeRegister = scan.nextInt();
 
         scan.close();
     }
--- Ativitys POO/ativityss/entities/EmployeeData.java
+++ Ativitys POO/ativityss/entities/EmployeeData.java
@@ -1,15 +1,34 @@
 package entities;
 
 public class EmployeeData {
-    private int employeeID;
+    private Integer employeeID;
     private String employeeName;
-    private double employeeWage;
+    private Double employeeWage;
 
-    public EmployeeData(int employeeID, String employeeName, double employeeWage){
+    public EmployeeData(Integer employeeID, String employeeName, Double employeeWage){
         this.employeeID = employeeID;
         this.employeeName = employeeName;
         this.employeeWage = employeeWage;
 
     }
+
+    public int getEmployeeID(){
+        return employeeID;
+    }
+
+    public void setEmployeeID(Integer empoloyeeID){
+        this.employeeID = employeeID;
+    }
     
+    public String getEmployeeName(){
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName){
+        this.employeeName = employeeName;
+    }
+
+    public double getEmployeeWage(){
+        return employeeWage;
+    }
 }
