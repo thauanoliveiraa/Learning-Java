284bdef2df0c676b34c930fb6b81c1f1a38854bd
Thauan de Oliveira Ramos <96343663+thauanoliveiraa@users.noreply.github.com>
2022-03-10 09:29:56 -0300

    Java POO

--- Ativitys POO/ativityss/application/atividade9.java
+++ Ativitys POO/ativityss/application/atividade9.java
@@ -2,11 +2,14 @@ package application;
 import java.util.Scanner;
 import java.util.Locale;
 import entities.EmployeeData;
+import java.util.List;
+import java.util.ArrayList;
 
 public class atividade9 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner scan = new Scanner(System.in);
+        List<EmployeeData> dataList = new ArrayList<>();
 
         /* Fazer um programa para ler um numero inteiro N e depois os dados (id, nome e salario) de N funcionarios. Nao deve haver repetição de id
         
@@ -16,6 +19,26 @@ public class atividade9 {
 
         System.out.print("How many employees wil be registered ? ");
         int employeeRegister = scan.nextInt();
+
+        for(int i =0; i<employeeRegister; i++){
+            System.out.print("\n\nInsert your ID : ");
+            int employeeID = scan.nextInt();
+
+            System.out.print("Insert your name : ");
+            String employeeName = scan.nextLine();
+            employeeName = scan.nextLine();
+
+            System.out.print("Insert your actual wage : ");
+            double employeeWage = scan.nextDouble();
+            List<EmployeeData> dataList = new ArrayList<>();
+
+            List.add();
+
+        }
+
+        System.out.print("\n\nRegister sucessfuly !!!");
+
+
 
         scan.close();
     }
