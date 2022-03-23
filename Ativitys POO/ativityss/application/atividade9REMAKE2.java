cf7588a18d7fdd42810c7e1e4de5c6a76041edf6
Thauan de Oliveira Ramos <96343663+thauanoliveiraa@users.noreply.github.com>
2022-03-23 09:57:21 -0300

    Java POO

--- Ativitys POO/ativityss/application/atividade9REMAKE2.java
+++ Ativitys POO/ativityss/application/atividade9REMAKE2.java
@@ -15,11 +15,94 @@ public class atividade9REMAKE2 {
         
         Lembre-se de aplicar a técnica de encapsulamento para não permitir que o salário possa ser mudado livremente. Um salario pode aumentado com base em uma operação de aumento por uma porcentagem */
         
+        List<EmployeeDataREMAKE2> dataList = new ArrayList<>();
+
+        int choice = 0;
         System.out.print("How many employees will be registered ? ");
         int employeeRegister = scan.nextInt();
 
-        
+        for(int i=0; i<employeeRegister; i++){
+            System.out.printf("\n\n\n-- %d REGISTER --", i+1);
+            System.out.print("\nInsert employee ID : ");
+            int employeeID = scan.nextInt();
+            while(checkID(dataList, employeeID)){
+                System.out.print("\n\nError, this id already exist, try again");
+                System.out.print("Insert employee ID : ");
+                employeeID = scan.nextInt();
+            }
+            System.out.print("\nInsert employee name : ");
+            scan.nextLine();
+            String employeeName = scan.nextLine();
+            System.out.print("\nInsert employee salary : ");
+            double employeeSalary = scan.nextDouble();
+            EmployeeDataREMAKE2 employee = new EmployeeDataREMAKE2(employeeID, employeeName, employeeSalary);
+            dataList.add(employee);
+        }
+        do{
+            System.out.print("\n\nWhat would you like to do now ? ");
+            System.out.print("1 - VIEW REGISTER\n2 - REMOVE REGISTER\n3 - CHANGE EMPLOYEE NAME\n4 - ADD SALARY PERCENTAGE\n5 - EXIT\n\nInsert your choice : ");
+            choice = scan.nextInt();
+            switch(choice){
+                case 1:
+                    System.out.print("\n\nVIEW REGISTER");
+                    System.out.print(dataList);
+                    System.out.print("View was sucessfully !!!");
+                    break;
+                
+                case 2:
+                    System.out.println("\n\nREMOVE REGISTER");
+                    System.out.print("Insert the position to remove all data : ");
+                    int registerPosition = scan.nextInt();
+                    int convertPosition = (registerPosition-1);
+                    dataList.remove(convertPosition);
+                    System.out.print("\nRemoved was sucessfully !!!");
+                    break;
+                
+                case 3:
+                    System.out.print("\n\nCHANGE EMPLOYEE NAME");
+                    System.out.print("Insert employee ID to change name : ");
+                    int employeeIDchangeName = scan.nextInt();
+                    EmployeeDataREMAKE2 employeeChangeName = dataList.stream().filter(x -> x.getEmployeeID() == employeeIDchangeName).findFirst().orElse(null);
+                    if(employeeChangeName== null){
+                        System.out.print("\nEmployee ID inserted doesnt exist, try again");
+                    }
+                    else{
+                        System.out.print("Insert new employee name : ");
+                        scan.nextLine();
+                        String newName = scan.nextLine();
+                        employeeChangeName.setEmployeeName(newName);
+                        System.out.print("\nName changed was sucessfully !!!");
+                    }
+                    break;
+
+                case 4:
+                    System.out.print("\n\nADD SALARY PERCENTAGE");    
+                    System.out.print("Insert the emmployee ID to add salary percentage : "); 
+                    int findID = scan.nextInt();
+                    EmployeeDataREMAKE2 addSalaryPercentage = dataList.stream().filter(x -> x.getEmployeeID() == findID).findFirst().orElse(null);
+                    if(addSalaryPercentage == null){
+                        System.out.print("\nError, the employee ID inserted doesnt exist");
+                    }                
+                    else{
+                        System.out.print("Insert the salary percentage : ");
+                        double salaryPercentage = scan.nextDouble();
+                        addSalaryPercentage.percentageSalaryCalc(salaryPercentage);
+                        System.out.printf("Percentage inserted was sucessfully to ID %d", findID);
+                    }
+                    break;
+                
+                case 5:
+                    System.out.print("\n\nThanks for using any services !!! Back later : )");    
+                    break;
+                }
+
+        }while(choice != 5);
 
         scan.close();
+    }
+
+    public static boolean checkID(List<EmployeeDataREMAKE2> dataList, int employeeID){
+        EmployeeDataREMAKE2 employeeCheckID = dataList.stream().filter(x -> x.getEmployeeID() == employeeID).findFirst().orElse(null);
+        return employeeCheckID != null;
     }
 }
--- Ativitys POO/ativityss/entities/EmployeeDataREMAKE2.java
+++ Ativitys POO/ativityss/entities/EmployeeDataREMAKE2.java
@@ -1,8 +1,43 @@
 package entities;
 
 public class EmployeeDataREMAKE2 {
-    private String employeeName;
     private Integer employeeID;
+    private String employeeName;
     private Double employeeSalary;
     private double percentage;
+
+    public EmployeeDataREMAKE2(Integer employeeID, String employeeName, Double employeeSalary){
+        this.employeeID = employeeID;
+        this.employeeName = employeeName;
+        this.employeeSalary = employeeSalary;
+    }
+
+    public Integer getEmployeeID(){
+        return employeeID;
+    }
+
+    public String getEmployeeName(){
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName){
+        this.employeeName = employeeName;
+    }
+
+    public Double getEmployeeSalary(){
+        return employeeSalary;
+    }
+
+    public void setEmployeeSalary(){
+        this.employeeSalary = employeeSalary;
+    }
+
+    public void percentageSalaryCalc(double percentage){
+        employeeSalary += employeeSalary*(percentage/100);
+    }
+
+    @Override
+    public String toString(){
+        return "  ID : " + employeeID + "  NAME : " + employeeName+ "  ACTUAL SALARY : " + String.format("R$ %.2f ", employeeSalary) + "\n\n";
+    }
 }
