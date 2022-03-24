04374dfb3ed1a649d5f5ccb3b417e65a1f61d0e6
Thauan de Oliveira Ramos <96343663+thauanoliveiraa@users.noreply.github.com>
2022-03-24 09:01:45 -0300

    Java POO

--- Ativitys POO/ativityss/application/atividade10.java
+++ Ativitys POO/ativityss/application/atividade10.java
@@ -1,7 +1,29 @@
 package application;
+import java.util.Scanner;
+import java.util.Locale;
+import entities.ArrayVerification;
+
 
 public class atividade10 {
     public static void main(String[] args) {
-        /* Fazer um programa para ler dois numeros inteiros M e N, e depois ler uma matriz de M linhas por N colunas contendo numeros inteiros, podeno haver repetições. Em seguida, ler um número inteiro X que pertence à matriz. Para cada ocorrência de X, mostrar os valores à esquerda, acima, à direita e abaixo de X, quando houver, conforme exemplo */
+        /* Fazer um programa para ler dois numeros inteiros M e N, e depois ler uma matriz de M linhas por N colunas contendo numeros inteiros, podendo haver repetições. Em seguida, ler um número inteiro X que pertence à matriz. Para cada ocorrência de X, mostrar os valores à esquerda, acima, à direita e abaixo de X, quando houver, conforme exemplo */
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+        int lineValue = 0;
+        int columValue = 0;
+    
+        ArrayVerification[][] array = new ArrayVerification[lineValue][columValue];
+
+        for(int i=0; i<lineValue; i++){
+            for(int j=0; i<columValue; i++){
+                System.out.print("Insert line value : ");
+                lineValue= scan.nextInt();
+        
+                System.out.print("Insert colum value : ");
+                columValue = scan.nextInt();
+            }
+        }
+
+        scan.close();
     }
 }
