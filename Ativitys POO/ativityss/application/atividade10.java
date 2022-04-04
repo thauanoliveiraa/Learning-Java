230a759bcf0358a4a6decb567cf40ebd3cd89844
Thauan de Oliveira Ramos <96343663+thauanoliveiraa@users.noreply.github.com>
2022-04-04 17:51:37 -0300

    Java POO

--- Ativitys POO/ativityss/application/atividade10.java
+++ Ativitys POO/ativityss/application/atividade10.java
@@ -1,43 +1,7 @@
 package application;
-import java.util.Scanner;
-import java.util.Locale;
-import entities.ArrayVerification;
-
 
 public class atividade10 {
     public static void main(String[] args) {
-        /* Fazer um programa para ler dois numeros inteiros M e N, e depois ler uma matriz de M linhas por N colunas contendo numeros inteiros, podendo haver repetições. Em seguida, ler um número inteiro X que pertence à matriz. Para cada ocorrência de X, mostrar os valores à esquerda, acima, à direita e abaixo de X, quando houver, conforme exemplo */
-        Locale.setDefault(Locale.US);
-        Scanner scan = new Scanner(System.in);
-        int i = 0;
-        int j = 0;
-
-        System.out.print("Insert quantity : ");
-        int n = scan.nextInt();
-
-        int[][] array = new int[n][n];
-
-
-        try{
-            for(i=0; i<n; i++){
-                for(j=0; i<n;  j++){
-                    System.out.print("Insert value : ");
-                    array[i][j] = scan.nextInt();
-        
-                }
-            }
-        }
-
-        catch(ArrayIndexOutOfBoundsException ae[]){
-            System.out.println("Também nao entendi" +ae.getMessage());	
-        }
-
-        finally{
-
-        }
-
-        //ArrayVerification arrayVerification = new ArrayVerification(lineValue, columValue);
-
-        scan.close();
+        /* faça um programa que preencha uma matriz 20x10 com numeros inteiros, e some cada uma das colunas, armazenando o resultado da soma em um vetor. A seguir o programa devera multiclar cada elemento da matriz pela soma da coluna e mostrar a matriz resultante. */
     }
 }
