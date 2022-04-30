5b1f03bc7920fb4d1869fc9274497ec80458df6d
Thauan de Oliveira Ramos <96343663+thauanoliveiraa@users.noreply.github.com>
2022-04-30 11:07:41 -0300

    Java POO

---
+++ Ativitys POO/ativityss/application/auroraRegisterSystem.java
@@ -0,0 +1,24 @@
+package application;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Locale;
+import java.util.List;
+import java.util.ArrayList;
+import entities.AuroraEngine;
+
+
+public class auroraRegisterSystem{
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        List<AuroraEngine> auroraList = new ArrayList<>(); //Criação da lista para armazenamento dos registros
+
+        //pedir dados como marca do carro, modelo do carro, placa do carro e valor do carro
+
+
+        System.out.print("Insira a marca do carro : ");
+        
+        
+    }
+}
