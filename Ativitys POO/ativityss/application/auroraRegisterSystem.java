037ffd6b7668abd039a389ce985cb4678a69520e
Thauan de Oliveira Ramos <96343663+thauanoliveiraa@users.noreply.github.com>
2022-05-03 11:44:31 -0300

    Java POO

--- Ativitys POO/ativityss/application/auroraRegisterSystem.java
+++ Ativitys POO/ativityss/application/auroraRegisterSystem.java
@@ -8,19 +8,28 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.time.Instant;
+import java.util.Calendar;
 import entities.AuroraEngine;
 
 
 public class auroraRegisterSystem{
-    public static void main(String[] args) throws IOException, InterruptedException, ParseException{
+    public static void main(String[] args) throws IOException, InterruptedException{
         Locale.setDefault(Locale.US);
         Scanner scan = new Scanner(System.in);
 
+        AuroraEngine aurora = new AuroraEngine();
+
+        // , ParseException
+
         List<AuroraEngine> auroraList = new ArrayList<>(); //Criação da lista para armazenamento dos registros
 
-        SimpleDateFormat auroraSystemDate = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        /* SimpleDateFormat auroraSystemDate = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
-        Date auroraDate = auroraSystemDate.parse("dd/MM/yyyy HH:mm:ss");
+        Date auroraDate = auroraSystemDate.parse("dd/MM/yyyy HH:mm:ss"); */
+
+        //Date auroraSystemDate = new Date();
+
+        Calendar c = Calendar.getInstance();
 
         //pedir dados como marca do carro, modelo do carro, placa do carro e valor do carro
 
@@ -36,11 +45,29 @@ public class auroraRegisterSystem{
                 Runtime.getRuntime().exec("clear");
         }                        
             Thread.sleep(3000);
-            System.out.print("Welcome "+loginName);
-            System.out.print("access in "+auroraSystemDate.format(auroraDate));
-        
+            int hora = c.get(Calendar.HOUR_OF_DAY);
 
+            int month = c.get(Calendar.MONTH);
 
-                
+            System.out.println("Mês: "+c.get(Calendar.MONTH));
+
+            if(hora > 6 && hora < 12){
+                System.out.println("Good Morning, welcome "+loginName);
+                //System.out.print("Welcome "+loginName);
+            }
+            else if(hora > 12 && hora < 18){
+                System.out.println("Good Afternoon, welcome "+loginName);
+            }
+            else{
+                System.out.println("Good Night, welcome "+loginName);
+            }
+            System.out.println("Access in "+aurora.getMonth());
+           
+            //System.out.println(auroraSystemDate);
+
+            System.out.println("Data/Hora atual: "+c.getTime());
+            System.out.println("Ano: "+c.get(Calendar.YEAR));
+            System.out.println("Mês: "+c.get(Calendar.MONTH));
+            System.out.println("Dia do Mês: "+c.get(Calendar.DAY_OF_MONTH));    
     }
 }
--- Ativitys POO/ativityss/entities/AuroraEngine.java
+++ Ativitys POO/ativityss/entities/AuroraEngine.java
@@ -5,7 +5,25 @@ public class AuroraEngine {
     private String loginPassword; //Senha correspondente
     private String carModel; //Modelo do carro
     private String carName;
+    private String month;
 
-    public String getString
+    /* public String getString */
+
+    public AuroraENGINE(String loginName, String loginPassword, String carModel, String carName, String month){
+        this.loginName = loginName;
+        this.loginPassword = loginPassword;
+        this.carModel = carModel;
+        this.carName = carName;
+    }
+
+    public String getMonth(){
+        if(month == "1"){
+            return "Janeiro";   
+        }
+        else if(month == "2"){
+            return "Fevereiro";
+        }
+        return month;
+    }
     
 }
